03a0042599ace07221b5462b3ea11a56a37bdf87
natanaelo-jr <135863264+natanaelo-jr@users.noreply.github.com>
2023-12-28 21:34:40 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillMember(){
+    return{
+        restrict: 'E',
+        templateUrl: 'modules/skills/views/member.html',
+        controller: 'SkillMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        
+    }
+}
